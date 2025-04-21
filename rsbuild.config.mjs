2b7eeb7ddc418e52290575afee414b30fbdf9232
Danniel Rolfe <talkto@dannielrolfe.com>
2025-04-20 18:37:15 -0700

    Setup github pages workflow

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -1,3 +1,7 @@
 import { defineConfig } from '@rsbuild/core';
 
-export default defineConfig({});
+export default defineConfig({
+    output: {
+        assetPrefix: '/snuggle-struggles-static'
+    }
+});
