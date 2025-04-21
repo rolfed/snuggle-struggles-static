c4dc4bffea6fcac5a2ed5dff0d7123217fa9ddd4
Danniel Rolfe <talkto@dannielrolfe.com>
2025-04-20 18:57:32 -0700

    Setup tailwind

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './index.css';
 
 document.querySelector('#root').innerHTML = `
 <div class="content">
-  <h1>Vanilla Rsbuild</h1>
-  <p>Start building amazing things with Rsbuild.</p>
 </div>
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
 `;
