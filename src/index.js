c534e128eec9c51d86786251dff5fbb5e35b5de8
Danniel Rolfe <talkto@dannielrolfe.com>
2025-04-21 21:56:04 -0700

    wip

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles/tailwind.css';
 import logo from './static/images/logo.png';
 import hero from './static/images/hero.jpg';
+import gsap from 'gsap';
 
 
 const logoImg = document.querySelector("#logo");
@@ -13,3 +14,6 @@ const heroEl = document.querySelector('#hero');
 if (heroEl) {
     heroEl.style.backgroundImage = `url(${hero})`;
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+});
